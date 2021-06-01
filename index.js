3e537943c1c167f974473ca59078dbd16f6ee2a7
dila93 <>
2021-06-01 17:43:11 +0000

    Day 20 of "30 days of code"

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const day18 = require('./queuesAndStacks.js');
 const day19 = require('./interfaces.js');
+const day20 = require('./sorting.js');
 
 mainTitle();
 
 loadDaySolution(day18, day18.excecute);
 loadDaySolution(day19, day19.excecute);
+loadDaySolution(day20, day20.excecute);
 
 function mainTitle() {
 	console.log("HACKER RANK");
---
+++ sorting.js
@@ -0,0 +1,82 @@
+/*
+	Given an array, , of size  distinct elements, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following  lines:
+
+	Array is sorted in numSwaps swaps.
+	where  is the number of swaps that took place.
+		First Element: firstElement
+	where  is the first element in the sorted array.
+		Last Element: lastElement
+	where  is the last element in the sorted array.
+
+	Hint: To complete this challenge, you will need to add a variable that keeps a running tally of all swaps that occur during
+ */
+
+class Sort{
+	constructor(){
+		this.firstElement = 0;
+		this.lastElement = 0;
+		this.numSwaps = 0;
+	}
+
+	bubbleSort(arr, n){
+		this.numSwaps = 0;
+		for (let i = 0; i < n; i++) {
+			for (let j = 0; j < n - 1; j++) {
+				const leftNumber = arr[j];
+				const rightNumber =  arr[j + 1];
+				if (this.isLeftGreatherThanRight(leftNumber, rightNumber)) {
+					arr = this.swapArrayPositions(arr, j);
+					this.numSwaps++;
+				}
+			}
+
+			this.firstElement = arr[0];
+			this.lastElement = arr[arr.length-1];
+			
+			if (this.numSwaps == 0) {
+				break;
+			}
+		}
+	}
+
+	swapArrayPositions(arr, currentIndex){
+		let temp = arr[currentIndex];
+		arr[currentIndex] = arr[currentIndex + 1];
+		arr[currentIndex + 1] = temp;
+		return arr;
+	}
+
+	isLeftGreatherThanRight(leftNumber, rightNumber){
+		return (leftNumber > rightNumber);
+	}
+
+	getSwaps(){
+		return this.numSwaps;
+	}
+
+	getFirstElement(){
+		return this.firstElement
+	}
+
+	getLastElement(){
+		return this.lastElement;
+	}
+
+}
+
+let main = ()=> {
+		let arrayOfNumbers = [3, 2, 1];
+		let arrayLength = arrayOfNumbers.length
+		let sorting = new Sort();
+		sorting.bubbleSort(arrayOfNumbers, arrayLength);
+		console.log("Array is sorted in", sorting.getSwaps(), "swaps.");
+		console.log("First Element:", sorting.getFirstElement());
+		console.log("Last Element:", sorting.getLastElement());
+	}
+
+module.exports = {
+	excecute: main,
+	day: 20,
+	title: "Sorting."
+};
+
